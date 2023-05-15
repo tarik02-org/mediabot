87fdf543111ab9c41587f1bd7239c696120680de
Tarik02 <Taras.Fomin@gmail.com>
2023-05-15 10:11:39 +0300

    add ability to pass Infinity to sleep function

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,9 +1,11 @@
 export function sleep(timeout: number, abortSignal: AbortSignal) {
     return new Promise<void>((resolve, reject) => {
-        const handle = setTimeout(() => {
-            abortSignal.removeEventListener('abort', onAbort);
-            resolve();
-        }, timeout);
+        const handle = isFinite(timeout) ?
+            setTimeout(() => {
+                abortSignal.removeEventListener('abort', onAbort);
+                resolve();
+            }, timeout) :
+            undefined;
 
         const onAbort = (reason: any) => {
             clearTimeout(handle);
