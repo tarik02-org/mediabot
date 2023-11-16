ac36e1acd16da673b0bcfc9e31556c012f0a7377
Tarik02 <Taras.Fomin@gmail.com>
2023-11-16 23:00:44 +0200

    fix

--- packages/parser-tiktok/tiktok.ts
+++ packages/parser-tiktok/tiktok.ts
@@ -21,6 +21,29 @@ const DEFAULT_HEADERS = {
     'Range': 'bytes=0-',
 };
 
+const imagePostSchema = z.object({
+    images: z.array(z.object({
+        imageURL: z.object({
+            urlList: z.array(z.string()),
+        }),
+        imageWidth: z.number(),
+        imageHeight: z.number(),
+    })),
+});
+
+const videoSchema = z.object({
+    width: z.number(),
+    height: z.number(),
+    duration: z.number(),
+    playAddr: z.string(),
+    downloadAddr: z.string(),
+    bitrateInfo: z.array(z.object({
+        PlayAddr: z.object({
+            UrlList: z.array(z.string()),
+        }),
+    })),
+});
+
 export const downloadFromTiktok = async (query: Query) => {
     const cookieJar = new CookieJar();
 
@@ -51,14 +74,36 @@ export const downloadFromTiktok = async (query: Query) => {
         }).text(),
     );
 
-    const {
-        __DEFAULT_SCOPE__: {
-            'webapp.video-detail': {
-                itemInfo: { itemStruct },
-                shareMeta,
+    const convertImagePost = (imagePost: z.TypeOf<typeof imagePostSchema>) => ({
+        images: imagePost.images.map(image => ({
+            width: image.imageWidth,
+            height: image.imageHeight,
+
+            download: async () => await got.get(image.imageURL.urlList[0], {
+                headers: {
+                    ...DEFAULT_HEADERS,
+                },
+                followRedirect: false,
+            }).buffer(),
+        })),
+    });
+
+    const convertVideo = (video: z.TypeOf<typeof videoSchema>) => ({
+        video: {
+            width: video.width,
+            height: video.height,
+            duration: video.duration,
+        },
+
+        downloadVideo: async () => await got.get(video.bitrateInfo[0].PlayAddr.UrlList[0], {
+            headers: {
+                ...DEFAULT_HEADERS,
             },
-        }
-    } = z.object({
+            followRedirect: false,
+        }).buffer(),
+    });
+
+    const universalDataForRehydrationParseResult = z.object({
         __DEFAULT_SCOPE__: z.object({
             'webapp.video-detail': z.object({
                 itemInfo: z.object({
@@ -72,32 +117,13 @@ export const downloadFromTiktok = async (query: Query) => {
                         }),
                         z.union([
                             z.object({
-                                imagePost: z.object({
-                                    images: z.array(z.object({
-                                        imageURL: z.object({
-                                            urlList: z.array(z.string()),
-                                        }),
-                                        imageWidth: z.number(),
-                                        imageHeight: z.number(),
-                                    })),
-                                }),
+                                imagePost: imagePostSchema,
                             }).transform(data => ({
                                 ...data,
                                 type: 'images' as const,
                             })),
                             z.object({
-                                video: z.object({
-                                    width: z.number(),
-                                    height: z.number(),
-                                    duration: z.number(),
-                                    playAddr: z.string(),
-                                    downloadAddr: z.string(),
-                                    bitrateInfo: z.array(z.object({
-                                        PlayAddr: z.object({
-                                            UrlList: z.array(z.string()),
-                                        }),
-                                    })),
-                                }),
+                                video: videoSchema,
                             }).transform(data => ({
                                 ...data,
                                 type: 'video' as const,
@@ -111,54 +137,92 @@ export const downloadFromTiktok = async (query: Query) => {
                 }),
             }),
         }),
-    }).parse(
-        JSON.parse(
-            dom.window.document.querySelector<HTMLScriptElement>('script#__UNIVERSAL_DATA_FOR_REHYDRATION__')!.textContent!,
-        ),
+    }).safeParse(
+        JSON.parse(dom.window.document.querySelector<HTMLScriptElement>('script#__UNIVERSAL_DATA_FOR_REHYDRATION__')?.textContent ?? '{}')
     );
 
-    const url = `https://www.tiktok.com/@${itemStruct.author.uniqueId}/video/${itemStruct.id}`;
-
-    switch (itemStruct.type) {
-        case 'images':
-            return {
-                title: shareMeta.desc,
-                url,
-
-                type: 'images' as const,
-
-                images: itemStruct.imagePost.images.map(image => ({
-                    width: image.imageWidth,
-                    height: image.imageHeight,
-
-                    download: async () => await got.get(image.imageURL.urlList[0], {
-                        headers: {
-                            ...DEFAULT_HEADERS,
-                        },
-                        followRedirect: false,
-                    }).buffer(),
-                })),
-            };
-
-        case 'video':
-            return {
-                title: shareMeta.desc,
-                url,
-
-                type: 'video' as const,
-
-                video: {
-                    width: itemStruct.video.width,
-                    height: itemStruct.video.height,
-                    duration: itemStruct.video.duration,
+    if (universalDataForRehydrationParseResult.success) {
+        const {
+            __DEFAULT_SCOPE__: {
+                'webapp.video-detail': {
+                    itemInfo: { itemStruct },
+                    shareMeta,
                 },
-    
-                downloadVideo: async () => await got.get(itemStruct.video.bitrateInfo[0].PlayAddr.UrlList[0], {
-                    headers: {
-                        ...DEFAULT_HEADERS,
-                    },
-                    followRedirect: false,
-                }).buffer(),
-            };
+            }
+        } = universalDataForRehydrationParseResult.data;
+        
+        const url = `https://www.tiktok.com/@${itemStruct.author.uniqueId}/video/${itemStruct.id}`;
+
+        switch (itemStruct.type) {
+            case 'images':
+                return {
+                    title: shareMeta.desc,
+                    url,
+                    type: 'images' as const,
+                    ...convertImagePost(itemStruct.imagePost),
+                };
+
+            case 'video':
+                return {
+                    title: shareMeta.desc,
+                    url,
+                    type: 'video' as const,
+                    ...convertVideo(itemStruct.video),
+                };
+        }
     }
+
+    const sigiParseResult = z.object({
+        ItemModule: z.record(
+            z.intersection(
+                z.object({
+                    id: z.string(),
+                    desc: z.string(),
+                    author: z.string(),
+                }),
+                z.union([
+                    z.object({
+                        imagePost: imagePostSchema,
+                    }).transform(data => ({
+                        ...data,
+                        type: 'images' as const,
+                    })),
+                    z.object({
+                        video: videoSchema,
+                    }).transform(data => ({
+                        ...data,
+                        type: 'video' as const,
+                    })),
+                ]),
+            ),
+        ),
+    }).safeParse(
+        JSON.parse(dom.window.document.querySelector<HTMLScriptElement>('script#SIGI_STATE')?.textContent ?? '{}')
+    );
+
+    if (sigiParseResult.success) {
+        const module = Object.values(sigiParseResult.data.ItemModule)[0];
+
+        const url = `https://www.tiktok.com/@${module.author}/video/${module.id}`;
+
+        switch (module.type) {
+            case 'images':
+                return {
+                    title: module.desc,
+                    url,
+                    type: 'images' as const,
+                    ...convertImagePost(module.imagePost),
+                };
+
+            case 'video':
+                return {
+                    title: module.desc,
+                    url,
+                    type: 'video' as const,
+                    ...convertVideo(module.video),
+                };
+        }
+    }
+
+    throw new Error(`universalDataForRehydration and sigiState failed to parse: ${universalDataForRehydrationParseResult.error.format()}, ${sigiParseResult.error.format()}`);
 };
